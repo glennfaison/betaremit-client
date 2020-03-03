8c2016e910fea98b568717c6ff50a4c9604459a9
glennfaison <glennfaison@gmail.com>
2020-03-03 21:09:27 +0100

    Create 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -16,12 +16,14 @@ export default function NotFound(props) {
         <h1>oops!</h1>
         <h2>Error 404 : Page Not Found</h2>
         <a href={Routes.products}>go back</a>
+        {/*
         <div className="notfound-social">
           <a href="."><i className="fa fa-facebook"></i></a>
           <a href="."><i className="fa fa-twitter"></i></a>
           <a href="."><i className="fa fa-pinterest"></i></a>
           <a href="."><i className="fa fa-google-plus"></i></a>
         </div>
+        */}
       </div>
 
     </div>
